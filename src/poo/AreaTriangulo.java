77bc614f014efe8b09f3708f1df2ce7ff8641920
Felipe Salgado <104802376+fesalgz@users.noreply.github.com>
2024-04-22 20:11:43 -0300

    atualizações da aula de POO
    mudança dos códigos adicionando mais elementos de POO

--- src/poo/AreaTriangulo.java
+++ src/poo/AreaTriangulo.java
@@ -25,11 +25,8 @@ public class AreaTriangulo {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double pX = (x.a + x.b + x.c)/2.0;
-        double areaX = Math.sqrt(pX * (pX - x.a) * (pX - x.b) * (pX - x.c));
-
-        double pY = (y.a + y.b + y.c)/2.0;
-        double areaY = Math.sqrt(pY * (pY- y.a) * (pY - y.b) * (pY - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("A área do Triângulo X é: %.2f %n", areaX);
         System.out.printf("A área do Triângulo Y é: %.2f %n", areaY);
